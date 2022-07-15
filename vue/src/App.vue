2b62b5a8bfa3a21382db215412531b148d4f9a0a
Seiki Rowins Bie <61301846+seikirowinsbie@users.noreply.github.com>
2022-07-16 07:25:26 +0800

    added navigation bar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-
+<script>
+import Navbar from './components/layout/Navbar.vue';
+import UserStore from './statemanagement/UserStore';
+export default {
+  components:{
+    Navbar
+  },
+  computed:{
+    isLoggedIn()
+    {
+      return UserStore.getters.isAuth;
+    }
+  }
+}
 </script>
 
 <template>
+<div>
+<Navbar v-if="isLoggedIn" />
   <router-view></router-view>
+</div>
 </template>
 
 <style>
---
+++ vue/src/components/layout/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">RCS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <router-link class="nav-link" to="users">Users</router-link>
+        </li>
+        <li class="nav-item">
+                  <router-link class="nav-link" to="users">Roles</router-link>
+
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Log Out</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+export default {
+   mounted()
+   {
+    console.log("yesh")
+   }
+}
+</script>
